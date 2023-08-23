a021d3e3c3c0aa7547f8f8874d0a16321d63f04f
Horoz <serelaw@mail.ru>
2023-08-23 19:56:07 +0300

    fix

--- routers/RSA.py
+++ routers/RSA.py
@@ -5,6 +5,6 @@ router = APIRouter(tags=["RSA"])
 
 
 @router.get("/RSA/encrypt", response_model=str)
-def encrypt_RSA(text: str, p: str, q: str):
-    public, private = generate_keypair(int(p), int(q))
+def encrypt_RSA(text: str, p: int, q: int):
+    public, private = generate_keypair(p, q)
     return encrypt(public_key=public, plaintext=text)
--- utils/Caesar.py
+++ utils/Caesar.py
@@ -1,5 +1,4 @@
-def caesar_cipher(text:str, key:str):
-  newkey = int(key)
+def caesar_cipher(text:str, key:int):
   result = ""
   for i in range(len(text)):
     char = text[i]
@@ -9,7 +8,7 @@ def caesar_cipher(text:str, key:str):
         base = ord('a')
       else:
         base = ord('A')
-      num = (num - base + newkey) % 26 + base
+      num = (num - base + key) % 26 + base
       result += chr(num)
     else:
       result += char
